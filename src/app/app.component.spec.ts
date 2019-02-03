38d11da2f2b8e32e1ea4c21c50e9a27474e2fdf6
LuisColumnaDeveloper <lmcolumna@gmail.com>
2019-02-03 13:29:17 -0600

    Primer commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'plantillaVelonic'`, () => {
+  it(`should have as title 'adminPro'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('plantillaVelonic');
+    expect(app.title).toEqual('adminPro');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to plantillaVelonic!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to adminPro!');
   });
 });
