3acad38bb6ed20d6ee0de9fb0e6869457739311c
LuisColumnaDeveloper <lmcolumna@gmail.com>
2019-02-05 22:07:26 -0600

    Funciones que retornen observables

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subscriber } from 'rxjs';
+
+import { retry } from "rxjs/operators";
+
 
 @Component({
   selector: 'app-rxjs',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RxjsComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    
+    this.regresaObservable().pipe(
+      retry(2)
+    )
+    .subscribe(
+      numero => console.log('Subs',numero),
+      error => console.error('Error',error),
+      () => console.log('El observador termino!')
+      );
+
+  }
 
   ngOnInit() {
+  }
+
+  regresaObservable():Observable< number >{
+    return  new Observable( (observer:Subscriber<number>) =>{
+
+      let contador =0;
+      let intervalo = setInterval(()=>{
+        
+        contador += 1;
+
+        observer.next(contador );
+
+        if(contador===3){
+          clearInterval(intervalo);
+          observer.complete();
+        }
+
+        if(contador===2){
+          //clearInterval(intervalo);
+          observer.error('Auxilio');
+        }
+
+      },1000)
+    });
   }
 
 }
