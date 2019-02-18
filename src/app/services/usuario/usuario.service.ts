7bd78f7923d795b975d67d254402d953fc15f35b
Luis Miguel <lmcolumna@ssp.df.gob.mx>
2019-02-18 15:50:22 -0600

    loginnormal, login google con error

--- src/app/services/usuario/usuario.service.ts
+++ src/app/services/usuario/usuario.service.ts
@@ -18,6 +18,40 @@ export class UsuarioService {
     
   }
 
+  loginGoogle(token:string){
+
+    let url = URL_SERVICIOS+'/login/google';
+
+    return this.http.post(url,{token});
+
+  }
+
+  login(usuario:Usuario, recordar:boolean=false){
+
+
+    if(recordar){
+
+      localStorage.setItem('email',usuario.email);
+
+    }else{
+      localStorage.removeItem('email');
+    }
+
+    let url = URL_SERVICIOS + '/login';
+    return this.http.post(url, usuario)
+                    .pipe(
+                      map(
+                        (resp:any) => {
+                      localStorage.setItem('id',resp.id);
+                      localStorage.setItem('token',resp.token);
+                      localStorage.setItem('usuario', JSON.stringify(resp.usuario));
+
+                      return true;
+                    }));
+    
+
+  }
+
   crearUsuario(usuario:Usuario){
     let url = URL_SERVICIOS + '/usuario';
 
